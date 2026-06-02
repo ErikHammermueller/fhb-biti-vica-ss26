07e6e9057726ef399d61d365c2054cabcb1d1e71
ErikHammermueller <erik16056@gmail.com>
2026-06-02 16:56:36 +0200

    Use unique resource names

--- Abgabe_2_ErikHammermueller_260602/infra/main.tf
+++ Abgabe_2_ErikHammermueller_260602/infra/main.tf
@@ -1,46 +1,50 @@
-# Ubuntu Template suchen
+# Ubuntu 24.04 Template suchen
 data "exoscale_template" "ubuntu" {
   zone = "at-vie-1"
   name = "Linux Ubuntu 24.04 LTS 64-bit"
 }
 
-# SSH Key registrieren
+# SSH-Key in Exoscale registrieren
 resource "exoscale_ssh_key" "erik" {
-  name       = "erik-key-2"
+  name       = "erik-hammermueller-key"
   public_key = var.ssh_public_key
 }
 
-# Firewall
+# Security Group für Webserver
 resource "exoscale_security_group" "web" {
-  name = "erik-abgabe2-sg"
+  name = "erik-hammermueller-sg"
 }
 
+# SSH Zugriff erlauben
 resource "exoscale_security_group_rule" "ssh" {
   security_group_id = exoscale_security_group.web.id
 
-  type        = "INGRESS"
-  protocol    = "TCP"
-  start_port  = 22
-  end_port    = 22
-  cidr        = "0.0.0.0/0"
+  type       = "INGRESS"
+  protocol   = "TCP"
+  start_port = 22
+  end_port   = 22
+
+  cidr = "0.0.0.0/0"
 }
 
+# HTTP Zugriff erlauben
 resource "exoscale_security_group_rule" "http" {
   security_group_id = exoscale_security_group.web.id
 
-  type        = "INGRESS"
-  protocol    = "TCP"
-  start_port  = 80
-  end_port    = 80
-  cidr        = "0.0.0.0/0"
+  type       = "INGRESS"
+  protocol   = "TCP"
+  start_port = 80
+  end_port   = 80
+
+  cidr = "0.0.0.0/0"
 }
 
-# VM
+# Virtuelle Maschine erstellen
 resource "exoscale_compute_instance" "vm" {
 
   zone = "at-vie-1"
 
-  name = "erik-abgabe2"
+  name = "erik-hammermueller-vm"
 
   template_id = data.exoscale_template.ubuntu.id
 
